d06a5eced77c15cb3de31c21f69c76274b9be35d
Anthony-Phiri-533-MW <anthonyphiri533@gmail.com>
2025-01-22 20:18:11 +0200

    change: app.run() function

--- app.py
+++ app.py
@@ -216,4 +216,5 @@ class Login(Resource):
 
 # Run the server
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host=os.getenv('HOST', '127.0.0.1'), port=int(os.getenv('PORT', 8080)), debug=False)
+
