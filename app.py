2edc48b620eb08c5e9493aa4d9e4e0128e375bd4
Anthony-Phiri-533-MW <anthonyphiri533@gmail.com>
2025-01-22 21:09:51 +0200

    changed host

--- app.py
+++ app.py
@@ -216,5 +216,5 @@ class Login(Resource):
 
 # Run the server
 if __name__ == '__main__':
-    app.run(host=os.getenv('HOST', '127.0.0.1'), port=int(os.getenv('PORT', 8080)), debug=False)
+    app.run(host=os.getenv('HOST', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=False)
 
